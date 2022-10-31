631590225c0fb0a4c53649811c5c2e9203aec76d
Jacob Dev <mercihabam@gmail.com>
2022-10-31 16:30:06 +0200

    set head icon

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return <>
   <Head>
     <title>NEAR dev 101 | Thegym nextjs challenge</title>
+    <link rel="shortcut icon" href="/app/near-logo.svg" type="image/x-icon" />
   </Head>
   <Layout><Component {...pageProps} /></Layout>
   </>
